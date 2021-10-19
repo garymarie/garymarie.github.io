0b1b0bee812c483519eccbd9d7425fc1c1a9befe
Marie <mfarrell@cs.nuim.ie>
2021-10-19 22:24:09 +0100

    email form

---
+++ form-to-email.php
@@ -0,0 +1,20 @@
+
+<?php
+  $name = $_POST['name'];
+  $message = $_POST['message'];
+
+  $email_from = 'garymarie@garymarie.com';
+
+  $email_subject = "New Form submission";
+
+  $email_body = "You have received a new message from the user $name.\n".
+                            "Here is the message:\n $message".
+
+  $to = "marie-farrell16@hotmail.com, genglishgroup@gmai.com";
+
+  $headers = "From: $email_from ";
+
+  mail($to,$email_subject,$email_body,$headers);
+?>
+
+
